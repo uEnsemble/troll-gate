23fb4eff9ac2df5fcf58268746f01435377a3b7e
Adam King <rak@us.ibm.com>
2017-01-03 11:35:04 -0500

    Post conductor

--- bin/afterDeploy.sh
+++ bin/afterDeploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "send event to github"
+echo "Trigger simple workflow"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: text/plain' -d '{ }' "http://$CONDUCTOR_SERVER:8080/api/workflow/simple_workflow"
