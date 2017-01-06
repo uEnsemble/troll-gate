2537a52f7a5000e151175547dbbd668b9a693d44
Travis CI <builds@travis-ci.com>
2017-01-06 16:40:40 -0500

    refactor tag calculation

--- bin/tagrelease.sh
+++ bin/tagrelease.sh
@@ -6,10 +6,11 @@ echo "$TRAVIS_BUILD_NUMBER"
 echo "$TRAVIS_REPO_SLUG"
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
-export GIT_TAG=build-$TRAVIS_BRANCH-$(date -u "+%Y-%m-%d")-$TRAVIS_BUILD_NUMBER
+TAG_DATE=$(date -u "+%Y-%m-%d")
+export GIT_TAG="build-$TRAVIS_BRANCH-$TAG_DATE-$TRAVIS_BUILD_NUMBER"
 echo -n $GIT_TAG > version
 git commit -a -m "Set build VERSION number to $GIT_TAG"
-echo "TravisCI build tagged with  $GIT_TAG"
+echo "TravisCI build tagged with $GIT_TAG"
 git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
 echo "build tagged. Now push to https://<GITHUB_OAUTH_TOKEN>@github.com/$TRAVIS_REPO_SLUG"
 git push --quiet --tags "https://$GITHUB_OAUTH_TOKEN@github.com/$TRAVIS_REPO_SLUG" > /dev/null 2>&1
