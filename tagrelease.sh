614b227222db7356b304f11ea9645c22ab2051b6
Travis CI <builds@travis-ci.com>
2017-01-06 18:06:49 -0500

    tag release in predeploy step

--- tagrelease.sh
+++ tagrelease.sh
@@ -8,10 +8,10 @@ git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 TAG_DATE=$(date -u "+%Y-%m-%d")
 export GIT_TAG="build-$TRAVIS_BRANCH-$TAG_DATE-$TRAVIS_BUILD_NUMBER"
-echo -n $GIT_TAG > version
-git commit -a -m "Set build VERSION number to $GIT_TAG"
-echo "TravisCI build tagged with $GIT_TAG"
+# echo -n $GIT_TAG > version
+# git commit -a -m "Set build VERSION number to $GIT_TAG"
 git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
-echo "build tagged. Now push to https://<GITHUB_OAUTH_TOKEN>@github.com/$TRAVIS_REPO_SLUG"
-git push --quiet --tags "https://$GITHUB_OAUTH_TOKEN@github.com/$TRAVIS_REPO_SLUG" > /dev/null 2>&1
-echo "tag pushed"
+echo "TravisCI build tagged with $GIT_TAG"
+# echo "build tagged. Now push to https://<GITHUB_OAUTH_TOKEN>@github.com/$TRAVIS_REPO_SLUG"
+# git push --quiet --tags "https://$GITHUB_OAUTH_TOKEN@github.com/$TRAVIS_REPO_SLUG" >/dev/null 2>&1
+# echo "tag $GIT_TAG pushed $?"
