27338fba672539b1fd989d4f010bf443fa947195
Travis CI <builds@travis-ci.com>
2017-01-06 11:33:00 -0500

    echo build tag name

--- bin/tagrelease.sh
+++ bin/tagrelease.sh
@@ -9,7 +9,7 @@ git config --global user.name "Travis CI"
 export GIT_TAG=build-$TRAVIS_BRANCH-$(date -u "+%Y-%m-%d")-$TRAVIS_BUILD_NUMBER
 # echo -n $GIT_TAG > version
 # git commit -a -m "Set build VERSION number to $GIT_TAG"
-echo "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
+echo "TravisCI build tagged with  $GIT_TAG"
 git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
 echo "build tagged. Now push to https://<GITHUB_OAUTH_TOKEN>@github.com/$TRAVIS_REPO_SLUG"
 git push --quiet --tags "https://$GITHUB_OAUTH_TOKEN@github.com/$TRAVIS_REPO_SLUG" > /dev/null 2>&1
