83bf6be7bad4797411df26bf030f274bcd97a50b
Travis CI <builds@travis-ci.com>
2017-01-06 16:09:09 -0500

    Add version file to take release version info

--- bin/tagrelease.sh
+++ bin/tagrelease.sh
@@ -7,8 +7,8 @@ echo "$TRAVIS_REPO_SLUG"
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 export GIT_TAG=build-$TRAVIS_BRANCH-$(date -u "+%Y-%m-%d")-$TRAVIS_BUILD_NUMBER
-# echo -n $GIT_TAG > version
-# git commit -a -m "Set build VERSION number to $GIT_TAG"
+echo -n $GIT_TAG > version
+git commit -a -m "Set build VERSION number to $GIT_TAG"
 echo "TravisCI build tagged with  $GIT_TAG"
 git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
 echo "build tagged. Now push to https://<GITHUB_OAUTH_TOKEN>@github.com/$TRAVIS_REPO_SLUG"
